87c1bd3bf99c2952d97edabc8da24be5286a330a
ydri <66348306+ydri1@users.noreply.github.com>
2024-04-07 21:42:50 -0300

    Update map.cpp

--- src/client/map.cpp
+++ src/client/map.cpp
@@ -815,10 +815,10 @@ void Map::setAwareRange(const AwareRange& range)
 void Map::resetAwareRange()
 {
     AwareRange range;
-    range.left = 8;
-    range.top = 6;
-    range.bottom = 7;
-    range.right = 9;
+    range.left = 14; //Change this to = maxClientViewportX
+    range.top = 8; //Change this to = maxClientViewportY
+    range.bottom = range.top+1;
+    range.right = range.left+1;
     setAwareRange(range);
 }
 
